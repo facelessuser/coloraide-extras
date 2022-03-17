bead631d2ce1e263a98e2b255b1ddcdbd08f5d12
facelessuser <faceless.shop@gmail.com>
2022-03-17 07:13:56 -0600

    Add an Extra Color class that has all default and extra colors

--- coloraide_extras/__init__.py
+++ coloraide_extras/__init__.py
@@ -1,2 +1,22 @@
 """ColorAide Extra."""
 from .__meta__ import __version_info__, __version__  # noqa: F401
+from coloraide import Color as Base
+from .spaces.hsi import HSI
+from .spaces.ipt import IPT
+from .spaces.igpgtg import IgPgTg
+from .spaces.cmy import CMY
+from .spaces.ucs import UCS
+from .spaces.uvw import UVW
+from .spaces.xyy import XyY
+from .spaces.hunter_lab import HunterLab
+
+__all__ = ("Color", "SPACES")
+
+SPACES = [HSI, IPT, IgPgTg, CMY, UCS, UVW, XyY, HunterLab]
+
+
+class Color(Base):
+    """Color class containing all default and extra color spaces."""
+
+
+Color.register(SPACES)
