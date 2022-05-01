cb4ef665dbedc71debdb88a244e9e0d5839fc7fc
facelessuser <faceless.shop@gmail.com>
2022-05-01 12:17:23 -0600

    Deployed 4797467 with MkDocs version: 1.3.0

--- playground-config-8626880a.js
+++
@@ -1,5 +0,0 @@
-var color_notebook = {
-    "playground_wheels": ['Pygments-2.11.2-py3-none-any.whl', 'coloraide-0.15.0.post1-py3-none-any.whl', 'coloraide_extras-0.4.0-py3-none-any.whl'],
-    "notebook_wheels": ['Markdown-3.3.4-py3-none-any.whl', 'pymdown_extensions-9.0-py3-none-any.whl', 'Pygments-2.11.2-py3-none-any.whl', 'coloraide-0.15.0.post1-py3-none-any.whl', 'coloraide_extras-0.4.0-py3-none-any.whl'],
-    "default_playground": "from coloraide_extras import Color\ncoloraide.__version__\nColor('color(--xyy 0.64 0.33 0.21264)')"
-}
