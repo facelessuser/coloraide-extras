c1f7294163adc0464dd0292b60f9bd40c8b18f77
facelessuser <faceless.shop@gmail.com>
2023-04-20 06:59:05 -0600

    Deployed e2ff18a with MkDocs version: 1.4.2

---
+++ playground-config-48e6b4a2.js
@@ -0,0 +1,5 @@
+var colorNotebook = {
+    "playgroundWheels": ['Pygments-2.12.0-py3-none-any.whl', 'coloraide-2.2.1-py3-none-any.whl', 'coloraide_extras-1.5-py3-none-any.whl'],
+    "notebookWheels": ['pyyaml', 'Markdown-3.3.4-py3-none-any.whl', 'pymdown_extensions-9.10-py3-none-any.whl', 'Pygments-2.12.0-py3-none-any.whl', 'coloraide-2.2.1-py3-none-any.whl', 'coloraide_extras-1.5-py3-none-any.whl'],
+    "defaultPlayground": "from coloraide_extras.everything import ColorAll as Color\ncoloraide.__version__\ncoloraide_extras.__version__\nColor('color(--ucs 0.27493 0.21264 0.12243 / 1)')"
+}
