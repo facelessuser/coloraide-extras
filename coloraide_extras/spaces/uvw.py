f2ac1b8c390444425b49c9691fee2f6fe7fe3a08
facelessuser <faceless.shop@gmail.com>
2023-05-30 21:18:32 -0600

    UVW to/from base methods should be instance methods

--- coloraide_extras/spaces/uvw.py
+++ coloraide_extras/spaces/uvw.py
@@ -60,14 +60,12 @@ class UVW(Space):
     )
     WHITE = WHITES['2deg']['D65']
 
-    @classmethod
-    def to_base(cls, coords: Vector) -> Vector:
+    def to_base(self, coords: Vector) -> Vector:
         """To XYZ."""
 
-        return uvw_to_xyz(coords, cls.white())
+        return uvw_to_xyz(coords, self.white())
 
-    @classmethod
-    def from_base(cls, coords: Vector) -> Vector:
+    def from_base(self, coords: Vector) -> Vector:
         """From XYZ."""
 
-        return xyz_to_uvw(coords, cls.white())
+        return xyz_to_uvw(coords, self.white())
